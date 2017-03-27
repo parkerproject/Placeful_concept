16a4a57df2be83d193b80bad73f7d880917f5b7b
Parker <parkerproject@users.noreply.github.com>
2017-03-27 14:47:48 -0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,10 +2,10 @@
 import * as firebase from 'firebase';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCwIR_oc-NI0QzytZBBKwZ46nkMofifb2g',
-  authDomain: 'placeful.firebaseio.com',
-  databaseURL: 'https://placeful.firebaseio.com',
-  storageBucket: 'firebase-placeful.appspot.com',
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  storageBucket: '',
 };
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
